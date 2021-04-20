5bfa3a724f54d0a2b8ffbdd53f51297affd3d81d
mikaelforeseeti <45554647+mikaelforeseeti@users.noreply.github.com>
2021-04-20 07:56:58 +0000

    Release 0.0.2 (#14)
    Tunings support

--- securicad/enterprise/__init__.py
+++ securicad/enterprise/__init__.py
@@ -20,7 +20,7 @@ from securicad.enterprise.scenarios import Scenario
 from securicad.enterprise.simulations import Simulation
 from securicad.enterprise.users import Role, User
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Foreseeti AB"
 
 
