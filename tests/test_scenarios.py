6654805d2bda652963d55f46c6a23c786c33d61a
mikaelforeseeti <45554647+mikaelforeseeti@users.noreply.github.com>
2021-05-27 08:23:21 +0000

    Scenario tests (#18)

--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import sys
+import uuid
 from pathlib import Path
 
 import pytest
@@ -25,12 +26,144 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 from securicad.enterprise.exceptions import StatusCodeException
 
 # isort: on
+def test_list_create_scenario(data, client, project):
+    assert client.scenarios.list_scenarios(project) == []
+    modelpath = Path(__file__).with_name("aws.sCAD")
+    with open(modelpath, mode="rb") as reader:
+        model_info = project.upload_scad_model(
+            filename="aws.sCAD", file_io=reader, description="descr"
+        )
+    name = str(uuid.uuid4())
+    description = str(uuid.uuid4())
+    scenario = client.scenarios.create_scenario(
+        project=project,
+        name=name,
+        model_info=model_info,
+        description=description,
+        tunings=[],
+    )
+    fetched = client.scenarios.list_scenarios(project)
+    assert len(fetched) == 1, fetched
+    fetched_scenario = fetched[0]
+    assert fetched_scenario.name == name
+    assert fetched_scenario.description == description
+
 
 # TODO:
-# test_list_scenarios()
-# test_get_scenario_by_tid()
-# test_get_scenario_by_name()
-# test_create_scenario()
-# test_scenario_update()
-# test_scenario_delete()
-# test_scenario_list_simulations()
+def test_get_scenario_by_tid(data, client, project):
+    modelpath = Path(__file__).with_name("aws.sCAD")
+    with open(modelpath, mode="rb") as reader:
+        model_info = project.upload_scad_model(
+            filename="aws.sCAD", file_io=reader, description="descr"
+        )
+    name = str(uuid.uuid4())
+    description = str(uuid.uuid4())
+    scenario = client.scenarios.create_scenario(
+        project=project,
+        name=name,
+        model_info=model_info,
+        description=description,
+        tunings=[],
+    )
+    fetched = client.scenarios.get_scenario_by_tid(project=project, tid=scenario.tid)
+    assert scenario.tid == fetched.tid
+    assert fetched.name == name
+    assert fetched.description == description
+
+
+def test_get_scenario_by_name(data, client, project):
+    modelpath = Path(__file__).with_name("aws.sCAD")
+    with open(modelpath, mode="rb") as reader:
+        model_info = project.upload_scad_model(
+            filename="aws.sCAD", file_io=reader, description="descr"
+        )
+    name = str(uuid.uuid4())
+    description = str(uuid.uuid4())
+    scenario = client.scenarios.create_scenario(
+        project=project,
+        name=name,
+        model_info=model_info,
+        description=description,
+        tunings=[],
+    )
+    fetched = client.scenarios.get_scenario_by_name(project=project, name=name)
+    assert scenario.tid == fetched.tid
+    assert fetched.name == name
+    assert fetched.description == description
+
+
+def test_scenario_update(data, client, project):
+    modelpath = Path(__file__).with_name("aws.sCAD")
+    with open(modelpath, mode="rb") as reader:
+        model_info = project.upload_scad_model(
+            filename="aws.sCAD", file_io=reader, description="descr"
+        )
+    name = str(uuid.uuid4())
+    description = str(uuid.uuid4())
+    scenario = client.scenarios.create_scenario(
+        project=project,
+        name=name,
+        model_info=model_info,
+        description=description,
+        tunings=[],
+    )
+    new_name = str(uuid.uuid4())
+    new_description = str(uuid.uuid4())
+    scenario.update(name=new_name, description=new_description)
+    fetched = client.scenarios.get_scenario_by_name(project=project, name=new_name)
+    assert scenario.tid == fetched.tid
+    assert fetched.name == new_name
+    assert fetched.description == new_description
+
+
+def test_delete_scenario(data, client, project):
+    assert client.scenarios.list_scenarios(project) == []
+    modelpath = Path(__file__).with_name("aws.sCAD")
+    with open(modelpath, mode="rb") as reader:
+        model_info = project.upload_scad_model(
+            filename="aws.sCAD", file_io=reader, description="descr"
+        )
+    name = str(uuid.uuid4())
+    description = str(uuid.uuid4())
+    scenario = client.scenarios.create_scenario(
+        project=project,
+        name=name,
+        model_info=model_info,
+        description=description,
+        tunings=[],
+    )
+    fetched = client.scenarios.list_scenarios(project)
+    fetched_scenario = fetched[0]
+    scenario.delete()
+    assert client.scenarios.list_scenarios(project) == []
+    with pytest.raises(StatusCodeException) as ex:
+        fetched_scenario.delete()
+    utils.assert_status_code_exception(
+        exception=ex.value,
+        status_code=404,
+        method="DELETE",
+        url=utils.get_url("scenarios"),
+        data={"error": f"Scenario {fetched_scenario.tid} not found"},
+    )
+
+
+def test_scenario_list_simulations(data, client, project):
+    assert client.scenarios.list_scenarios(project) == []
+    modelpath = Path(__file__).with_name("aws.sCAD")
+    with open(modelpath, mode="rb") as reader:
+        model_info = project.upload_scad_model(
+            filename="aws.sCAD", file_io=reader, description="descr"
+        )
+    name = str(uuid.uuid4())
+    description = str(uuid.uuid4())
+    scenario = client.scenarios.create_scenario(
+        project=project,
+        name=name,
+        model_info=model_info,
+        description=description,
+        tunings=[],
+    )
+    fetched = scenario.list_simulations()
+    assert len(fetched) == 1, fetched
+    fetched_simulation = fetched[0]
+    assert fetched_simulation.name == "Initial simulation"
