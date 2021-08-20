89ff91036ad7ca3506ad5b0998ec9966632d33f1
Max Wällstedt (foreseeti) <48990854+maxwalls@users.noreply.github.com>
2021-08-20 12:24:46 +0200

    Update git hook for formatting JSON files (#40)

--- tools/git-hooks/pre-commit.d/format-json.sh
+++ tools/git-hooks/pre-commit.d/format-json.sh
@@ -13,8 +13,13 @@ echo_changed_json() {
 format_json() {
   echo "Formatting $(echo "$changed_json" | wc -l) json files"
   echo "$changed_json" | while IFS= read -r file; do
-    # shellcheck disable=SC2005
-    echo "$(jq . "$file")" >"$file"
+    if jq empty "$file" >/dev/null 2>&1; then
+      # shellcheck disable=SC2005
+      echo "$(jq . "$file")" >"$file"
+    else
+      >&2 printf "%s: %s\n" "$file" "$(jq empty "$file" 2>&1)"
+      exit 1
+    fi
   done
   echo "$changed_json" | tr "\n" "\0" | xargs -0 git add -f
 }
