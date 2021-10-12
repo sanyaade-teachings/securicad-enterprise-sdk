0d7ab4de207e0cfebb259ecd0d40ff80b4a76375
mikaelforeseeti <45554647+mikaelforeseeti@users.noreply.github.com>
2021-10-12 09:38:05 +0000

    More tests for user module (#44)
    * More tests for user module
    
    * More fixtures

--- tests/test_users.py
+++ tests/test_users.py
@@ -22,6 +22,7 @@ import utils
 # isort: off
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
+from securicad.enterprise.users import Role
 from securicad.enterprise.exceptions import StatusCodeException
 
 # isort: on
@@ -151,11 +152,168 @@ def test_change_password(data):
     assert_change_password_fails(client, "old_password", "new_password")
 
 
-# TODO:
-# test_list_users()
-# test_get_user_by_uid()
-# test_get_user_by_username()
-# test_create_user()
-# test_user_update()
-# test_user_delete()
-# test_user_set_role()
+def verify_user_exists(users, username, firstname, lastname, organization=None):
+    for user in users:
+        if user.username != username:
+            continue
+        assert user.organization == organization
+        assert user.firstname == firstname
+        assert user.lastname == lastname
+        return True
+    return False
+
+
+def test_list_users_sysadmin(client):
+    users = client.users.list_users()
+    assert len(users) == 8
+    users = sorted(users, key=lambda u: u.username)
+    assert verify_user_exists(
+        users, username="admin", firstname="admin", lastname="admin"
+    )
+    assert verify_user_exists(
+        users,
+        username="org1_admin",
+        firstname="org1",
+        lastname="admin",
+        organization="org1",
+    )
+    assert verify_user_exists(
+        users, username="org1_pc", firstname="org1", lastname="pc", organization="org1"
+    )
+    assert verify_user_exists(
+        users,
+        username="org1_user",
+        firstname="org1",
+        lastname="user",
+        organization="org1",
+    )
+    assert verify_user_exists(
+        users,
+        username="org2_admin",
+        firstname="org2",
+        lastname="admin",
+        organization="org2",
+    )
+    assert verify_user_exists(
+        users, username="org2_pc", firstname="org2", lastname="pc", organization="org2"
+    )
+    assert verify_user_exists(
+        users,
+        username="org2_user",
+        firstname="org2",
+        lastname="user",
+        organization="org2",
+    )
+
+
+def test_list_users_orgadmin(data):
+    org1 = data["organizations"]["org1"]
+    admin = [
+        userdata
+        for name, userdata in org1["users"].items()
+        if userdata["role"] == Role.ADMIN
+    ][0]
+    client = utils.get_client_org(
+        admin["username"], admin["password"], organization="org1"
+    )
+
+    users = client.users.list_users()
+    assert len(users) == 3
+    assert verify_user_exists(
+        users,
+        username="org1_admin",
+        firstname="org1",
+        lastname="admin",
+        organization="org1",
+    )
+    assert verify_user_exists(
+        users, username="org1_pc", firstname="org1", lastname="pc", organization="org1"
+    )
+    assert verify_user_exists(
+        users,
+        username="org1_user",
+        firstname="org1",
+        lastname="user",
+        organization="org1",
+    )
+
+
+def test_get_user_by_uid(client):
+    users = client.users.list_users()
+    org1_admin = [u for u in users if u.username == "org1_admin"][0]
+    fetched = client.users.get_user_by_uid(org1_admin.uid)
+    assert fetched.uid == org1_admin.uid
+    assert fetched.username == org1_admin.username
+    assert fetched.firstname == org1_admin.firstname
+    assert fetched.lastname == org1_admin.lastname
+    assert fetched.role == org1_admin.role
+
+
+def test_get_user_by_username(client):
+    users = client.users.list_users()
+    org1_admin = [u for u in users if u.username == "org1_admin"][0]
+    fetched = client.users.get_user_by_username(org1_admin.username)
+    assert fetched.uid == org1_admin.uid
+    assert fetched.username == org1_admin.username
+    assert fetched.firstname == org1_admin.firstname
+    assert fetched.lastname == org1_admin.lastname
+    assert fetched.role == org1_admin.role
+
+
+def test_create_user(client):
+    prevcount = len(client.users.list_users())
+    org2 = client.organizations.get_organization_by_name(name="org2")
+    client.users.create_user(
+        username="a",
+        firstname="b",
+        lastname="c",
+        password="pazzw00t",
+        organization=org2,
+        role=Role.ADMIN,
+    )
+    assert len(client.users.list_users()) == prevcount + 1
+
+
+def test_user_update(client):
+    users = client.users.list_users()
+    org1_admin = [u for u in users if u.username == "org1_admin"][0]
+    # keep old values
+    oldfirstname = org1_admin.firstname
+    oldlastname = org1_admin.lastname
+
+    org1_admin.update(username="abc", firstname="def", lastname="ghi")
+    newlist = client.users.list_users()
+    abc = [u for u in newlist if u.username == "abc"]
+    assert len(abc) == 1
+    org1_admin.update(
+        username="org1_admin", firstname=oldfirstname, lastname=oldlastname
+    )
+
+
+def test_user_delete(client):
+    prevcount = len(client.users.list_users())
+    org2 = client.organizations.get_organization_by_name(name="org2")
+    user = client.users.create_user(
+        username="a2",
+        firstname="b2",
+        lastname="c2",
+        password="pazzw00t",
+        organization=org2,
+        role=Role.ADMIN,
+    )
+    user.delete()
+    assert len(client.users.list_users()) == prevcount
+
+
+def test_user_set_role(client):
+    users = client.users.list_users()
+    org1_pc = [u for u in users if u.username == "org1_pc"][0]
+    assert org1_pc.role == Role.PROJECT_CREATOR
+    # set
+    org1_pc.set_role(Role.USER)
+    # verify
+    users2 = client.users.list_users()
+    org1_pc2 = [u for u in users2 if u.username == "org1_pc"][0]
+    assert org1_pc2.role == Role.USER
+    # reset
+    org1_pc.set_role(Role.PROJECT_CREATOR)
